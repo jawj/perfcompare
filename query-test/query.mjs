8918c31184a5660ed392ae53aac40ecb0cc91866
George MacKerron <george@mackerron.co.uk>
2023-08-10 18:00:00 +0100

    Tested and simplified usage example

---
+++ query-test/query.mjs
@@ -0,0 +1,23 @@
+import { types, neon } from '@neondatabase/serverless';  // or from 'pg'
+import { parse } from 'json-custom-numbers';
+
+// this leaves most values as ordinary numbers, using BigInt for large integers only
+function parseJSONWithBigInts(str) {
+  return parse(str, undefined, function (str) {
+    const n = +str;
+    const safe = n >= Number.MIN_SAFE_INTEGER && n <= Number.MAX_SAFE_INTEGER;
+    return safe || /[.eE]/.test(str) ? n : BigInt(str);
+  });
+}
+
+types.setTypeParser(types.builtins.JSON, parseJSONWithBigInts);
+types.setTypeParser(types.builtins.JSONB, parseJSONWithBigInts);
+
+
+const sql = neon(process.env.DATABASE_URL);
+
+sql`SELECT to_json((1e15)::bigint) AS big`
+  .then(x => console.log(x));
+
+sql`SELECT to_json((1e16 + 1)::bigint) AS big`
+  .then(x => console.log(x));
